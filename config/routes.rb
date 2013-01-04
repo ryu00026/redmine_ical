c9ffd287e3e858c79b7f7446f417822268c044a4
Ryuichi Kato <ryu00026@gmail.com>
2013-01-04 16:11:20 +0900

    redmine 2対応

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
-ActionController::Routing::Routes.draw do |map|
-  map.connect "ical_settings/:action", :controller => "ical_settings"
-  map.connect "exports/:action", :controller => "exports"
+RedmineApp::Application.routes.draw do
+  resources :ical_settings do
+    collection do
+      post "update_key"
+    end
+  end
+  resources :exports do
+    collection do
+      get "ical"
+    end
+  end
 end
